7c39b31e60e07fc6ce04e8dbdc7e7268215c050d
lantost <lantos.tibor@kifu.gov.hu>
2019-03-22 22:52:58 +0100

    FIX: webpack path issue

---
+++ src/containers/TimeSheet.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TimeSheet extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TimeSheet</h1>
+      </div>
+    );
+  }
+}
+
+export default TimeSheet;
--- webpack.common.js
+++ webpack.common.js
@@ -3,18 +3,24 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: './src/index.jsx',
-  },
+  entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  resolve: {
+    extensions: ['.*', '.jsx', '.js'],
+    modules: [
+      path.resolve('./src/'),
+      'node_modules',
+      '/public/',
+    ],
   },
   module: {
     rules: [
 
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: [/'node_modules'/],
         use: [{
           loader: 'babel-loader',

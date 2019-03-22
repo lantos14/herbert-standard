e4802ad40bb4c2638e3b758ade48962f2e12eb75
lantost <lantos.tibor@kifu.gov.hu>
2019-03-22 13:46:01 +0100

    FEAT: add webpack, babel, eslint

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import App from './components/App';
+
+import './styles/main.scss';
+
+reactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.jsx',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        exclude: [/'node_modules'/],
+        use: [{
+          loader: 'babel-loader',
+        }]
+      },
+
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'index.html'
+    }),
+    new CleanWebPackPlugin(),
+  ],
+};
